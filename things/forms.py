7e2af8f6866ae3c43351d26c23986daa014072e1
Huzaifa Afzal <muhammad.h.afzal@kcl.ac.uk>
2022-12-09 15:40:19 +0000

    Complete Task to render form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
 
-# Create your forms here.
+from .models import Thing
+from django.core.validators import RegexValidator
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea() , 'quantity': forms.NumberInput()}
